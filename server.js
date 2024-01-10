d2e98287cfaabdc50621d62d2364ce3dad8c55fb
justinluce <luce@midlandu.com>
2024-01-09 19:23:06 -0600

    adds database connection

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-// Imports express
+// Imports express, mysql, cors, and bcrypt
 const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+// Creates our connection with the database info
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'giphy'
+});
+
+// Connects to the database
+connection.connect();
 
 // Creates a new instance of express (creating our server)
 const app = express();
+// Allows our server to bypass cors errors
+// and also parses data as a json
+app.use(cors());
+app.use(express.json());
 
 // When the user sends a GET request to "/" it sends "Hello World!" back
 app.get("/", (req, res) => res.send("Hello World!"));
@@ -18,8 +36,16 @@ app.get("/users/:userId", (req, res) => {
 app.post("/register", async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        res.send(hashedPassword);
-        // In the future, we're going to have to save the password/username to the database
+        connection.query(
+            "INSERT INTO users VALUES (?, ?, ?)",
+            [1, req.body.username, hashedPassword],
+            function (err, rows, fields) {
+                if (err) throw err;
+                console.log("Rows: ", rows);
+                console.log("Fields: ", fields);
+            }
+        )
+        res.send("It worked");
     } catch (err) {
         res.send(err);
     }
@@ -28,5 +54,5 @@ app.post("/register", async (req, res) => {
 // When they make any request to any url that isn't "/", redirect them to "/"
 app.all("*", (req, res) => res.redirect("/"));
 
-// Starts the server on port 3000
-app.listen(3000, () => console.log("Server is listening on port 3000"));
+// Starts the server on port 3006
+app.listen(3006, () => console.log("Server is listening on port 3006"));
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,11 +3,29 @@ import { useUserContext } from '../context/UserContext';
 
 const LoginPage = () => {
     const [inputUsername, setInputUsername] = useState("");
+    const [password, setPassword] = useState("");
     const { setUser } = useUserContext();
 
     const handleLogin = () => {
         setUser(inputUsername);
     };
+
+    const handleRegister = async () => {
+        setUser(inputUsername);
+        const data = {
+            username: inputUsername,
+            password: password
+        }
+        console.log(data);
+        const response = await fetch("http://localhost:3006/register", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        console.log(response);
+    }
 
     return (
         <div>
@@ -18,10 +36,12 @@ const LoginPage = () => {
                 placeholder='Username'
             />
             <input 
+                onChange={e => setPassword(e.target.value)}
                 type='password'
                 placeholder='Password'
             />
             <button onClick={handleLogin}>Login</button>
+            <button onClick={handleRegister}>Register</button>
         </div>
     );
 };
